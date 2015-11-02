7a44752204dfb05e3e31d55341f3c78c564d6f20
reddo-br <an46e@svk.jp>
2015-11-02 23:49:59 +0900

    preferenceファイルの保存方法改善

--- src/pref/prefbase.rb
+++ src/pref/prefbase.rb
@@ -1,11 +1,18 @@
+# -*- coding: utf-8 -*-
 
 require 'json'
 require 'pathname'
+require 'fileutils'
+require 'util'
 
 class Prefbase
   def initialize( path )
     @path = Pathname.new(path) # jruby-9.0.0.0 windows problem
     @m    = Mutex.new
+
+    @tmpdir = Util.get_appdata_pathname / "tmp"
+    FileUtils.mkdir_p( @tmpdir )
+
   end
   
   def set( key ,val )
@@ -29,7 +36,16 @@ class Prefbase
   private
   def load_hash
     if @path.exist?
-      JSON.load( File.read(@path) )
+      begin
+        o = JSON.load( File.read(@path) )
+        if o
+          o
+        else
+          {}
+        end
+      rescue
+        {}
+      end
     else
       {}
     end
@@ -38,13 +54,12 @@ class Prefbase
   def save_hash(h)
     @m.synchronize{
       begin
-        open( @path , 'a'){|f|
-          if f.flock( File::LOCK_EX | File::LOCK_NB )
-            f.truncate(0)
-            f.write( JSON.pretty_generate( h ))
-            f.flock( File::LOCK_UN )
-          end
-        }
+
+        temp = Tempfile.create( "tmp" , @tmpdir )
+        temp.write( JSON.pretty_generate( h ))
+        temp.close # windows - 閉じないと移動できない
+        FileUtils.mv( temp.path , @path , {:force => true})
+        
       rescue
         $stderr.puts $!
         $stderr.puts $@
