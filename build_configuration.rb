6836fa0555ddde90c8e0987c90d1aef652824e9d
reddo-br <an46e@svk.jp>
2015-11-06 19:21:23 +0900

    jvmオプションの調整

--- build_configuration.rb
+++ build_configuration.rb
@@ -76,7 +76,7 @@ configuration do |c|
 	# Undocumented option 'jvm_arguments'
 	# default value: ""
 	#
-	c.jvm_arguments = "-splash:splash.png -Dfile.encoding=utf-8 -Xmx750m -XX:+UseConcMarkSweepGC"
+	c.jvm_arguments = "-splash:splash.png -Dfile.encoding=utf-8 -Xmx750m -XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC"
 
 	# Undocumented option 'java_library_path'
 	# default value: ""
--- misc/reddo.sh
+++ misc/reddo.sh
@@ -4,4 +4,4 @@ cd `dirname "$0"`
 SCRIPT_DIR=$(cd "$CDIR"; pwd)
 
 cd "$SCRIPT_DIR"
-java -Dfile.encoding=utf-8 -Xmx1g -XX:+UseConcMarkSweepGC -splash:splash.png -jar reddo.jar "$@"
+java -Dfile.encoding=utf-8 -Xmx1g -XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -splash:splash.png -jar reddo.jar "$@"
