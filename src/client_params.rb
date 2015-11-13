4f549ab88dda6b74f15a37033a1e446d8c9c1c39
reddo-br <an46e@svk.jp>
2015-11-13 22:32:03 +0900

    0.2.9

--- src/client_params.rb
+++ src/client_params.rb
@@ -1,9 +1,8 @@
 
 module ClientParams
-  # CLIENT_ID = "4Iz62cW_nVC_CQ"
   CLIENT_ID = "HDlQF7URpcuXgQ"
   REDIRECT_URI = "http://127.0.0.1:32323/auth_redirect"
-  VERSION = "0.2.8"
+  VERSION = "0.2.9"
   APP_NAME = "Reddo"
   USER_AGENT = "#{APP_NAME} #{VERSION}"
 end
