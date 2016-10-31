1186e386d4b1b869518cb41eea27934957b921f6
reddo-br <an46e@svk.jp>
2016-10-31 23:16:27 +0900

    imgurのサムネイルのurlを修正

--- src/builtin_scripts/thumb/imgur.rb
+++ src/builtin_scripts/thumb/imgur.rb
@@ -4,7 +4,7 @@ class ImgurThumb < ThumbnailScript
     if m = url.match( %r!https?://(?:i\.)?imgur\.com/([^\. /]+)[^ /]*$! )
       id = m[1]
       # "https://i.imgur.com/#{id}s.jpg" # 90x90
-      "https://i.imgur.com/#{id}m.jpg" # 
+      "https://i.imgur.com/#{id}t.jpg" # 
     else
       nil
     end
